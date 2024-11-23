0d00a11bb935190a1c9bacfba2b00291581f7ae9
Aanya1130 <timinpabo@gmail.com>
2024-11-23 22:30:15 +0530

    Add files via upload

---
+++ Intro To JS/script.js
@@ -0,0 +1,39 @@
+document.getElementById("myheading").innerHTML = "Heading from Javascript";
+
+//Add content in JavaScript
+document.getElementById("myparagraph").innerHTML = "Paragraph from Javascript";
+
+//Creating Variable
+name1 = "JavaScript"; //String Data type
+num1 = 34; //Number Data type
+num2 = 10; //Number Data type
+b = false; //Boolean Data type
+
+//Operators : Arithmetic Operators
+addition = num1 + num2;
+document.write(addition);
+document.write("<br>")
+subtraction = num1 - num2;
+document.write(subtraction);
+document.write("<br>")
+modulus = num1 % num2; //Remainder of the numbers
+document.write(modulus);
+
+//Operators : Assignment Operators
+num3 = 6;
+a = 10;
+b = 5;
+
+//Addition Assignment 
+a += b
+// a = a + b
+b += a
+// b = b + 1
+
+//Subtraction Assignment 
+a -= b
+// a = a - b
+
+a *= b
+a /= b
+a %= b
